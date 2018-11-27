52496553fef0f1ac9619080fe216a09c045025c5
finex1 <finex@youths4youths.com>
2018-11-27 11:37:57 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    'appsignature' => 'none', 
+	'clientid' => 'spa0o9fmqsbsplila9e6720z',
+	'clientsecret' => 'DvL7gAMqFCmaMcfrUowUl7Hk',
+	'defaultwsdl' => 'https://webservice.s7.exacttarget.com/etframework.wsdl',
+    'xmlloc' => '/some/path/to/cache/ExactTargetWSDL.xml',
+    'baseUrl' => 'https://www.exacttargetapis.com',
+    'baseAuthUrl' => 'https://auth.exacttargetapis.com',
+    'baseSoapUrl' => 'https://webservice.s7.exacttarget.com/Service.asmx',
+    'proxyhost' => 'localhost',
+    'proxyport' => '8080',
+    'proxyusername' => '',
+    'proxypassword' => '',	
+);
+
